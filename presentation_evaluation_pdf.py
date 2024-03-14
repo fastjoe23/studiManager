b8e8d43d200d12d23f78e1016282d3def98de3dd
fastjoe23 <fastjoe23@hotmail.com>
2024-03-14 15:16:28 +0100

    Assignment Formular changed

--- presentation_evaluation_pdf.py
+++ presentation_evaluation_pdf.py
@@ -1,11 +1,23 @@
 from fpdf import FPDF
 from fpdf.enums import XPos, YPos
 
+
 class EvaluationCreationError(Exception):
     pass
 
+
 class PresentationEvaluationPDF(FPDF):
-    def __init__(self, save_path,  evaluation_type, course_name, topic, lecturers, student, date, time):
+    def __init__(
+        self,
+        save_path,
+        evaluation_type,
+        course_name,
+        topic,
+        lecturers,
+        student,
+        date,
+        time,
+    ):
         super().__init__()
         self.save_path = save_path
         self.topic = topic
@@ -18,12 +30,36 @@ class PresentationEvaluationPDF(FPDF):
 
     def header(self):
         self.set_font("helvetica", "B", 13)
-        title = f"Bewertung {self.evaluation_type} Kurs {self.course_name}"
-        self.cell(w=0, h=10, text=title, border=0, new_x=XPos.LEFT, new_y=YPos.NEXT, align="C")
+        # Abkürzung für Art der Prüfung
+        if self.evaluation_type == "Bachelorarbeit":
+            abbrevation_type = "BA"
+        elif self.evaluation_type == "Projektarbeit 1":
+            abbrevation_type = "PA 1"
+        elif self.evaluation_type == "Projektarbeit 2":
+            abbrevation_type = "PA 2"
+        else:
+            abbrevation_type = self.evaluation_type
+
+        title = f"Bewertung mündliche {abbrevation_type}-Prüfung"
+        self.cell(
+            w=0, h=10, text=title, border=0, new_x=XPos.LEFT, new_y=YPos.NEXT, align="C"
+        )
+        self.cell(
+            w=0,
+            h=10,
+            text=f"Kurs {self.course_name}",
+            border=0,
+            new_x=XPos.LEFT,
+            new_y=YPos.NEXT,
+            align="C",
+        )
 
     def chapter_title(self, title):
         self.set_font("helvetica", "B", 12)
-        self.cell(w=0, h=10, text=title, border=0, new_x=XPos.LEFT, new_y=YPos.NEXT, align="L")
+        max_width = 180  # Maximale Breite der Zelle
+        self.multi_cell(
+            w=max_width, h=10, text=title, border=0, align="L", new_x=XPos.LEFT
+        )
         self.set_font("helvetica", "", 12)
 
     def create_evaluation(self):
@@ -31,20 +67,24 @@ class PresentationEvaluationPDF(FPDF):
             self.add_page()
 
             self.chapter_title(f"Titel: {self.topic}")
-            self.chapter_title(f"Vortragende: {self.student_name}")
+            self.chapter_title(f"Vortragende/r: {self.student_name}")
             self.chapter_title(f"Datum: {self.date}, {self.time} Uhr")
-            self.chapter_title(f"Prüfer: {', '.join(str(x) for x in self.lecturers)} ")
+            self.chapter_title(
+                f"Gutachter*in: {', '.join(str(x) for x in self.lecturers)} "
+            )
             self.ln(3)
             self.chapter_title("(A) Präsentation")
 
             self.set_font("helvetica", "", 11)
-            with self.table(col_widths=(60, 60, 50, 60), borders_layout="NONE") as table:
+            with self.table(
+                col_widths=(60, 60, 50, 60), borders_layout="NONE"
+            ) as table:
                 row = table.row()
                 row.cell("Vortragsstil", colspan=4)
                 row = table.row()
                 row.cell("(Umfang und Rhetorik)")
                 row.cell("spannend / überzeugend")
-                row.cell("                     O---O---O---O---O")
+                row.cell("O---O---O---O---O")
                 row.cell("langatmig/zäh und wenig überzeugend")
 
                 row = table.row()
@@ -54,7 +94,7 @@ class PresentationEvaluationPDF(FPDF):
                 row = table.row()
                 row.cell("(Qualität + Logik)")
                 row.cell("kompetenter Eindruck, durchgehend logisch")
-                row.cell("                     O---O---O---O---O")
+                row.cell("O---O---O---O---O")
                 row.cell("kein kompetenter Eindruck, roter Faden fehlt")
 
                 row = table.row()
@@ -64,17 +104,21 @@ class PresentationEvaluationPDF(FPDF):
                 row = table.row()
                 row.cell("(Folien + Handout)")
                 row.cell("gut aufbereitet, gut verständlich")
-                row.cell("                     O---O---O---O---O")
+                row.cell("O---O---O---O---O")
                 row.cell("schlecht aufbereitet, wenig verständlich")
 
             self.ln(5)
             self.chapter_title("(B) Diskussion")
             self.set_font("helvetica", "", 11)
-            with self.table(col_widths=(60, 60, 50, 60), borders_layout="NONE", first_row_as_headings=False) as table:
+            with self.table(
+                col_widths=(60, 60, 50, 60),
+                borders_layout="NONE",
+                first_row_as_headings=False,
+            ) as table:
                 row = table.row()
                 row.cell("Antwortverhalten")
                 row.cell("kompetente Antworten")
-                row.cell("                     O---O---O---O---O")
+                row.cell("O---O---O---O---O")
                 row.cell("wenig überzeugend")
 
             self.ln(5)
@@ -85,15 +129,12 @@ class PresentationEvaluationPDF(FPDF):
             self.ln(30)
             self.chapter_title("(D) Note:")
             self.ln()
-            self.cell(text="Unterschriften", align="L")
-            signature_line = "____________       "
-            signature_lines = ""
-            for _ in self.lecturers:
-                signature_lines += signature_line
-            signature_lines += "\n"
-            self.multi_cell(w=500, text=signature_lines)
-
-            pdf_file_path = self.save_path + f"/{str(self.course_name)}_{str(self.evaluation_type).replace(' ','')}_{str(self.student_name).replace(' ','')}_Bewertungsbogen.pdf"
+            self.cell(text="Unterschriften:", align="L")
+            
+            pdf_file_path = (
+                self.save_path
+                + f"/{str(self.course_name)}_{str(self.evaluation_type).replace(' ','')}_{str(self.student_name).replace(' ','')}_Bewertungsbogen.pdf"
+            )
             self.output(pdf_file_path)
             return pdf_file_path
 
